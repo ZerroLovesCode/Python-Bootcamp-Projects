7dd020c055cf8794af51137f042b3a652665f380
Zee <88308160+ZerroLovesCode@users.noreply.github.com>
2022-04-14 19:40:21 +0530

    Password generator
    Now accepts user input for getting the number of elements of each type.

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,17 +1,21 @@
+from ntpath import join
 import random
+from tokenize import Special
 AlphaList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 NumList = ['0','1', '2', '3', '4', '5', '6', '7', '8','9']
-Splchars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '<', '>', '?', '/', '.', ',', '_','-','+','=','`','~']
-l = int(input('How long do you want the password to be? '))
+Splchars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '<', '>', '?', '/', '.', ',', '_','-','+','=','`','~']
+numA = int(input('How many letters in the pasword? '))
+numN = int(input('How many numbers in the password? '))
+numS = int(input('How many special characters in the password? '))
+pw = []
 
-pw = ''
+for i in range(numA):
+    pw.append(random.choice(AlphaList))
+for i in range(numN):
+    pw.append(random.choice(NumList))
+for i in range(numS):
+    pw.append(random.choice(Splchars))    
 
-for i in range(l):
-    listChooser = random.randint(0,2)
-    if listChooser == 0:
-        pw += random.choice(AlphaList)
-    elif listChooser == 1:
-        pw += random.choice(NumList)
-    else:
-        pw += random.choice(Splchars)
-print(f'Your Password is: {pw}')
+random.shuffle(pw)
+passW = "".join(pw)
+print(f'Your Password is: {passW}')
