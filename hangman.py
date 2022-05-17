d3071dcc40ccc4db45e23b7b77003dd8423e996b
Sachit <sachitnair2002@gmail.com>
2022-05-17 12:02:47 +0530

    Base model

---
+++ hangman.py
@@ -0,0 +1,24 @@
+import random
+
+num_lives = 5
+word_list = ['words', 'for', 'testing', 'this', 'game']
+secret_word = random.choice(word_list)
+current_word = ['_']*len(secret_word)
+str_so_far = ''.join(current_word)
+while(num_lives > 0):
+    flag = False
+    print(str_so_far,'\n',sep=' ')
+    guess = input('Enter a letter: ')
+    for i, letter in enumerate(secret_word):
+        if guess == letter:
+            current_word[i] = guess
+            flag = True
+    str_so_far = ''.join(current_word)
+    if not flag:
+        num_lives -= 1
+        print(f'\nThe letter is not present in the word!\nYou have {num_lives} lives remaining')
+        if num_lives == 0:
+            print(f'\nSorry, you have lost. The word was "{secret_word}"')
+    if str_so_far == secret_word:
+        print(f'\nCongrats! You have won! The secret word was "{str_so_far}"')
+        break
