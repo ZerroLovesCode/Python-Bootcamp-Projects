a57c91571c099c2d128cbc2c638ff0b88bede7ee
Sachit <sachitnair2002@gmail.com>
2022-05-17 12:47:24 +0530

    Completed the game, need a better wordlist.

--- hangman.py
+++ hangman.py
@@ -80,14 +80,22 @@ word_list = ['words', 'for', 'testing', 'this', 'game']
 secret_word = random.choice(word_list)
 current_word = ['_']*len(secret_word)
 str_so_far = ''.join(current_word)
-
+guessed_correct_letters = set()
+print(stages[num_lives])
 while(num_lives > 0):
     
     flag = False
+    print('\n')
     print(' '.join(current_word),'\n',sep=' ')
     guess = input('Enter a letter: ')
+
+    if guess in guessed_correct_letters:
+        print('You have already guessed this letter!')
+        continue
+
     for i, letter in enumerate(secret_word):
         if guess == letter:
+            guessed_correct_letters.add(guess)
             current_word[i] = guess
             flag = True
     str_so_far = ''.join(current_word)
@@ -97,6 +105,8 @@ while(num_lives > 0):
         if num_lives == 0:
             print(f'\nSorry, you have lost. The word was "{secret_word}"')
     if str_so_far == secret_word:
+        print('\n')
+        print(' '.join(current_word))
         print(f'\nCongrats! You have won! The secret word was "{str_so_far}"')
         break
     print(stages[num_lives])
