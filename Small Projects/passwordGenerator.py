a5c8056aa9587e0c4602f006f6e868521fc788fa
Sachit <sachitnair2002@gmail.com>
2022-06-14 17:26:19 +0530

    Password generator!

--- Small Projects/passwordGenerator.py
+++ Small Projects/passwordGenerator.py
@@ -4,7 +4,7 @@ from tokenize import Special
 AlphaList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 NumList = ['0','1', '2', '3', '4', '5', '6', '7', '8','9']
 Splchars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '<', '>', '?', '/', '.', ',', '_','-','+','=','`','~']
-numA = int(input('How many letters in the pasword? '))
+numA = int(input('How many letters in the password? '))
 numN = int(input('How many numbers in the password? '))
 numS = int(input('How many special characters in the password? '))
 pw = []
